42b7364147da5caff6ddd167825eeab09fec2077
Guiguiprog <33095251+Guiguiprog@users.noreply.github.com>
2021-06-29 10:31:27 +0200

    Add mongodb connection and store API name and version

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 
-const dotenv = require('dotenv');
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+const mongoose = require('mongoose');
+//Connect to db
+mongoose.connect(process.env.DB_MONGO_CONNECT, {useNewUrlParser: true}, () =>
+    console.log("connected to database")
+);
+//######### Display name and version ############// 
+const apiinf = require('./models/apiinfo')
+var pjson = require('./package.json');
+console.log("name : " + pjson.name);
+console.log("version : " + pjson.version);
+const apiinfos = apiinf.findOneAndUpdate({name: pjson.name}, {version : pjson.version}, {upsert: true}).exec()
+//################################################//
 
 //Import routes
 const authRoute = require('./routes/routes')
